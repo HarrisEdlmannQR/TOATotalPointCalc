feb64597131f0633ac6c92d4fdc217c695a8db67
HarrisEdlmannQR <harris.edlmann@quickrelease.co.uk>
2025-02-07 14:45:55 +0000

    feat: main

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from functions import get_rewards, estimate_points
+from src.functions import (
+    minimise_residuals,
+    estimate_points,
+    estimate_purple_chance,
+)
 
-def run(raid_level):
-    points, purp_chance, quantities = get_rewards(estimate_points(raid_level=raid_level), raid_level=raid_level)
-    print(points, purp_chance, quantities)
+def run():
+    average_raid_level = minimise_residuals()
+    average_points = estimate_points(average_raid_level)
+    average_purple_chance = estimate_purple_chance(average_raid_level)
+
+    print(f"Based of given observations, the average raid level is {average_raid_level}, giving ~{average_points} "
+          f"points and a purple chance of ~{round(average_purple_chance*100, 3)}% "
+          f"(or 1/{round(100/(average_purple_chance*100), 3)}).")
 
 if __name__ == "__main__":
-    raid_level = 370
-    run(raid_level)
+    run()
